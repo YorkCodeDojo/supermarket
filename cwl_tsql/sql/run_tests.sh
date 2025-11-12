39282e6c7a90cfbbfef424747f11e92b50ce25b8
Andrew Stewart Gibson <andrew@commonwealthlabs.co.uk>
2025-11-12 21:05:27 +0000

    feat: add product price

--- cwl_tsql/sql/run_tests.sh
+++ cwl_tsql/sql/run_tests.sh
@@ -1,11 +1,12 @@
 PASSWORD='Password1!'
+SERVER='::1'
 TEST_DIR="./tests"
 
 for test_file in "$TEST_DIR"/*.sql; do
   test_name=$(basename "$test_file")
 
   # Run test
-  if sqlcmd -S ::1 -d supermarket -C -U sa -P "$PASSWORD" -b -i "$test_file" > /tmp/sql_test_output 2>&1; then
+  if sqlcmd -S "$SERVER" -d supermarket -C -U sa -P "$PASSWORD" -b -i "$test_file" > /tmp/sql_test_output 2>&1; then
     echo "_/ PASS: $test_name"
   else
     echo " X FAIL: $test_name"
--- cwl_tsql/sql/seed/merge_products.sql
+++ cwl_tsql/sql/seed/merge_products.sql
@@ -5,13 +5,15 @@ MERGE INTO
     dbo.Product AS tgt
 USING
     (VALUES
-        ('Milk'),
-        ('Bread')
-    ) AS src([Name])
+        ('Milk', 1.5),
+        ('Bread', 3.5)
+    ) AS src([Name], Price)
 ON tgt.[Name] = src.[Name]
+WHEN MATCHED THEN
+    UPDATE SET Price = src.Price
 WHEN NOT MATCHED BY TARGET THEN
-    INSERT ([Name])
-    VALUES (src.[Name])
+    INSERT ([Name], Price)
+    VALUES (src.[Name], src.Price)
 OUTPUT $action
 INTO @SummaryOfChanges
 ;
